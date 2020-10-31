a7dd949de590cbf1efa989ecb61445457f83341e
Matthias Griebel <matthias.griebel@uni-wuerzburg.de>
2020-10-31 11:34:22 +0000

    tests and pytorch 1.7 support

--- deepflash2/metrics.py
+++ deepflash2/metrics.py
@@ -13,7 +13,7 @@ from fastcore.foundation import patch
 class Dice_f1(Dice):
     "Dice coefficient metric for binary target in segmentation"
     def accumulate(self, learn):
-        pred,targ = flatten_check(learn.pred.argmax(dim=self.axis), learn.y[0])
+        pred,targ = flatten_check(learn.pred.argmax(dim=self.axis), learn.yb[0])
         self.inter += (pred*targ).float().sum().item()
         self.union += (pred+targ).float().sum().item()
 
