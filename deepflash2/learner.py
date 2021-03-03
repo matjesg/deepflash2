29ecc22f4d44a629f7f4800ab6c66a5c064bce4c
Matthias Griebel <matthias.griebel@uni-wuerzburg.de>
2021-03-03 12:06:42 +0000

    adding preproc option to learner

--- deepflash2/learner.py
+++ deepflash2/learner.py
@@ -221,10 +221,11 @@ def predict_tiles(self:Learner, ds_idx=1, dl=None, path=None, mc_dropout=False, 
 class EnsembleLearner(GetAttr):
     _default = 'config'
     def __init__(self, image_dir='images', mask_dir=None, config=None, path=None, ensemble_dir=None, remove_overlap=True,
-                 label_fn=None, metrics=None, loss_fn=None, cbs=None, ds_kwargs={}, stats=None, files=None):
+                 label_fn=None, metrics=None, loss_fn=None, cbs=None, ds_kwargs={}, stats=None, files=None, preproc_dir=None):
 
         self.config = config or Config()
         self.stats = stats
+        self.preproc_dir = preproc_dir
         self.path = Path(path) if path is not None else Path('.')
         self.metrics = metrics or [Iou()] #Dice_f1()
         self.loss_fn = loss_fn or WeightedSoftmaxCrossEntropy(axis=1)
@@ -254,7 +255,7 @@ class EnsembleLearner(GetAttr):
         self.models = {}
         self.recorder = {}
         self._set_splits()
-        self.ds = RandomTileDataset(self.files, label_fn=self.label_fn, remove_overlap=remove_overlap, **self.mw_kwargs, **self.ds_kwargs)
+        self.ds = RandomTileDataset(self.files, label_fn=self.label_fn, remove_overlap=remove_overlap, preproc_dir=self.preproc_dir, **self.mw_kwargs, **self.ds_kwargs)
         self.in_channels = self.ds.get_data(max_n=1)[0].shape[-1]
         self.df_val, self.df_ens, self.df_model, self.ood = None,None,None,None
 
@@ -295,8 +296,8 @@ class EnsembleLearner(GetAttr):
         self.stats = self.stats or self.ds.compute_stats()
         name = self.ensemble_dir/f'{self.arch}_model-{i}.pth'
         files_train, files_val = self.splits[i]
-        train_ds = RandomTileDataset(files_train, label_fn=self.label_fn, n_jobs=n_jobs, verbose=verbose, **self.mw_kwargs, **self.ds_kwargs)
-        valid_ds = TileDataset(files_val, label_fn=self.label_fn, n_jobs=n_jobs, verbose=verbose, **self.mw_kwargs,**self.ds_kwargs)
+        train_ds = RandomTileDataset(files_train, label_fn=self.label_fn, n_jobs=n_jobs, verbose=verbose, preproc_dir=self.preproc_dir, **self.mw_kwargs, **self.ds_kwargs)
+        valid_ds = TileDataset(files_val, label_fn=self.label_fn, n_jobs=n_jobs, verbose=verbose, preproc_dir=self.preproc_dir, **self.mw_kwargs,**self.ds_kwargs)
         batch_tfms = [Normalize.from_stats(*self.stats), WeightTransform(self.out_size, **self.mw_kwargs)]
         dls = DataLoaders.from_dsets(train_ds, valid_ds, bs=bs, after_item=self.item_tfms, after_batch=batch_tfms)
         pre = None if self.pretrained=='new' else self.pretrained
@@ -484,7 +485,7 @@ class EnsembleLearner(GetAttr):
     def lr_find(self, files=None, bs=None, n_jobs=-1, verbose=1, **kwargs):
         bs = bs or self.bs
         files = files or self.files
-        train_ds = RandomTileDataset(files, label_fn=self.label_fn, n_jobs=n_jobs, verbose=verbose, **self.mw_kwargs, **self.ds_kwargs)
+        train_ds = RandomTileDataset(files, label_fn=self.label_fn, n_jobs=n_jobs, verbose=verbose, preproc_dir=self.preproc_dir, **self.mw_kwargs, **self.ds_kwargs)
         dls = DataLoaders.from_dsets(train_ds,train_ds, bs=bs)
         pre = None if self.pretrained=='new' else self.pretrained
         model = torch.hub.load(self.repo, self.arch, pretrained=pre, n_classes=dls.c, in_channels=self.in_channels)
